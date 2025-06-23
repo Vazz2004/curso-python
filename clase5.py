f671f4cae0ed771ecccec66453bd92885c5ffb47
Samuel Vasquez <samivazqueles@gmail.com>
2025-06-22 23:47:52 -0500

    Add clase5.py to demonstrate variable types and file path printing

---
+++ clase5.py
@@ -0,0 +1,22 @@
+x = 10
+y = 5.678
+z= 1.2e6
+a= 1.26-6
+is_true = True
+is_false = False
+
+
+print(type(x))
+print(type(y))
+
+print(z)
+print(a)
+
+
+
+print(type(is_false))
+print(type(is_false))
+
+
+#imprimir archivo 
+print("La ruta de archivo es: C:\\Users\\Usuario\\Desktop\\archivo.txt")
