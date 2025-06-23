4f71ca93fe6687f689b330c1bc9bb427cbf1a0cc
Samuel Vasquez <samivazqueles@gmail.com>
2025-06-22 23:55:45 -0500

    Add clase6.py to demonstrate basic arithmetic operations in Python

---
+++ clase6.py
@@ -0,0 +1,19 @@
+a = 10
+b = 3
+
+print("suma:",a + b)
+print("resta:",a - b)
+print("multiplicacion:",a * b)
+print("divicion:",a / b)
+
+
+print("modulo", a % b)
+print("parte entera de la divivcion", a // b)
+print("potenciacion", a ** b)
+
+
+
+a += 2 
+
+
+print(a)
