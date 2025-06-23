e258ee373571cf7398a1b3c1edd7b941c39b72fb
Samuel Vasquez <samivazqueles@gmail.com>
2025-06-23 12:10:47 -0500

    Add clase13.py to demonstrate loops, conditionals, and basic data manipulation in Python

---
+++ clase13.py
@@ -0,0 +1,33 @@
+numbers = [1,2,3,45,6]
+
+for i in numbers:
+    print('aqui i es igual a: ',i+1)
+
+
+for i in range(3,10):
+    print(1)
+
+
+fruits = ["manzana", "Pera", "Naranja", "Uva"]
+
+for fruit in fruits :
+    print(fruit)
+    if fruit == "Naranja":
+        print('La fruta  naranja fue encontrada ')
+
+
+x = 0 
+
+while x<5:
+    if x ==3:
+      break
+    print(x)
+    x+=1
+
+
+numbers = [1,2,3,45,6]
+
+for i in numbers:
+    if i ==3:
+        continue
+    print('aqui i es igual a: ',i+1)
