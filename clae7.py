ce82bd38907958c5a7da6bfe6c148e0494cff0c9
Samuel Vasquez <samivazqueles@gmail.com>
2025-06-23 10:21:58 -0500

    Add clae7.py to demonstrate user input and type checking in Python

---
+++ clae7.py
@@ -0,0 +1,22 @@
+#Funcion input 
+
+nombre = input('ingrese su nombre: ')
+print(nombre)
+
+
+edad = input('ingrese su edad:  ')
+print(edad)
+
+
+#type
+
+nombre = input('ingrese su nombre: ')
+print(type(nombre))
+
+
+# se declara que el valor es un int
+
+edad = int(input('ingrese su edad:  ')) 
+print(type(edad))
+
+
