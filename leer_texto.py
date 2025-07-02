dd19fd6f87fcea670563be7e1287846e76dc6274
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-01 22:07:04 -0500

    Agregar archivo cuento.txt con la historia de Caperucita Roja y actualizar leer_texto.py para manejar la escritura en el archivo.

---
+++ leer_texto.py
@@ -0,0 +1,22 @@
+import limpiar_consola
+limpiar_consola.limpiarConsola()
+
+#Leer un archivo linea por linea
+'''with open('cuento.txt','r') as file:
+    for lineas in file:
+        print(lineas)'''
+
+
+#Leer todas las lieneas en una lista
+'''with open('cuento.txt','r') as file:
+    lines = file.readlines()
+    print(lines)'''
+
+
+#Escribir al final
+'''with open('cuento.txt','a') as file:
+    file.write("holaaaa")'''
+
+#Sobre escribir el texto 
+with open('cuento.txt','w') as file:
+    file.write('Hola')
