eaf922b618564c451c64cd46bd576d73807659f2
Samuel Vasquez <samivazqueles@gmail.com>
2025-07-06 14:58:37 -0500

    Agregar impresión de color aleatorio y barajar cartas en ramdom.py

--- ramdom.py
+++ ramdom.py
@@ -8,3 +8,13 @@ print(ramdom_number)
 colors = ['Rojo','Azul','Verde']
 random_color = random.choice(colors)
 print(random_color)
+
+
+
+#Baraja lcartas 
+
+cards= ['As', 'Rey', 'Reina']
+
+random.shuffle(cards)
+
+print(cards)
